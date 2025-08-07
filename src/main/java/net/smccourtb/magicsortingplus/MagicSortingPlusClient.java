25939a36913967f1e8e6fd72c16dcc74047d4a88
Shawnathan McCourt <shawnmccourtb@gmail.com>
2025-08-07 13:49:47 -0400

    initial commit

---
+++ src/main/java/net/smccourtb/magicsortingplus/MagicSortingPlusClient.java
@@ -0,0 +1,11 @@
+package net.smccourtb.magicsortingplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MagicSortingPlusClient extends MagicSortingPlus implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
